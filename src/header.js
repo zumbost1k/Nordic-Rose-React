b4ba4a34ecb0a32bdd184eec6ed8755bdbafc8d2
zumbost1k <matusevich.mi@gmail.com>
2023-04-27 18:33:38 +0300

    my first react commit

---
+++ src/header.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function Header() {
+  return (
+    <div className="App">
+      <header className="App-header"></header>
+      <h1 className='titl'>hello world</h1>
+    </div>
+  );
+}
+
+export default Header;
