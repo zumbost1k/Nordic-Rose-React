2a2a79fd9a8ec47b0430948329bb1eb666ea14d1
zumbost1k <matusevich.mi@gmail.com>
2023-05-01 13:04:00 +0300

    add router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Header from './header/header';
 import TopSectiom from './top_section/top_section.jsx';
 import SectionPhoto from './section_photo/section_photo.jsx';
 import Footer from './footer/footer.jsx';
+import { BrowserRouter, Route } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- src/section_photo/section_photo.jsx
+++ src/section_photo/section_photo.jsx
@@ -5,43 +5,50 @@ const photos = [
     {
         path: "photos/1.jpg",
         text: " Granny gives everyone the finger, and other tips from OFFF Barcelona",
-        alt: "Granny gives everyone the finger"
+        alt: "Granny gives everyone the finger",
+        href:"",
     },
 
     {
         path: "photos/2.jpg",
         text: "Hello world, or, in other words, why this blog exists",
-        alt: "Hello world"
+        alt: "Hello world",
+        href:"",
+        
     },
 
     {
         path: "photos/3.jpg",
         text: "Here are some things you should know regarding how we work",
-        alt: "regarding how we work"
+        alt: "regarding how we work",
+        href:"",
     },
 
     {
         path: "photos/4.jpg",
         text: "Updating list of 50+ sources on distributed teams, remote work, and how to make it all work better",
-        alt: "remote work"
+        alt: "remote work",
+        href:"",
     },
 
     {
         path: "photos/5.jpg",
         text: "A few words about this blog platform, Ghost, and how this site was made",
-        alt: "A few words about this blog platform"
+        alt: "A few words about this blog platform",
+        href:"",
     },
 
     {
         path: "photos/6.jpg",
         text: "How modern remote working tools get along with Old SchoolCowboy's methods",
-        alt: "modern remote working"
+        alt: "modern remote working",
+        href:"",
     },
 ]
 
 function Photo({ item }) {
     return (
-        <div className="atribute">
+        <a href={item.href} className="atribute">
             <img className="second_page_photos" width="304" height="176" src={item.path}
                 alt={item.alt} />
             <div>
@@ -49,7 +56,7 @@ function Photo({ item }) {
                     {item.text}
                 </p>
             </div>
-        </div>
+        </a>
     );
 }
 class SectionPhoto extends React.Component {
